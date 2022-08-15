44bbfc471695d229edb2a88df3d3bf8787266d20
triledev <tri.le.mail@gmail.com>
2022-08-15 12:47:17 -0500

    Initial Commit

---
+++ DemoFeature/ApiClient.swift
@@ -0,0 +1,19 @@
+//
+//  ApiClient.swift
+//  DemoFeature
+//
+//  Created by Tri Le on 8/15/22.
+//
+
+import Foundation
+
+class ApiClient {
+    private static let instance = ApiClient()
+    
+    static func getInstance()
+    private init() {}
+}
+
+let client = ApiClient()
+
+
