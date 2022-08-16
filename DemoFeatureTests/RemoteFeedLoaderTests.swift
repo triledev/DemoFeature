db796cff9bd7bf22ba54b0fdedd7b7efe37009cd
triledev <tri.le.mail@gmail.com>
2022-08-15 23:47:09 -0500

    Delivers invalidData error on 200 HTTP response with invalid json

--- DemoFeatureTests/RemoteFeedLoaderTests.swift
+++ DemoFeatureTests/RemoteFeedLoaderTests.swift
@@ -32,9 +32,9 @@ class RemoteFeedLoaderTests: XCTestCase {
 
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
-        var capturedErrors = [RemoteFeedLoader.Error]() // Actual Result
-        sut.load { capturedErrors.append($0) }
         let clientError = NSError(domain: "Test", code: 0)
+        var capturedErrors = [RemoteFeedLoader.Error]()
+        sut.load { capturedErrors.append($0) }
         client.complete(with: clientError)
         XCTAssertEqual(capturedErrors, [.connetivity])
     }
@@ -48,6 +48,15 @@ class RemoteFeedLoaderTests: XCTestCase {
             client.complete(withStatusCode: code, at: index)
             XCTAssertEqual(capturedErrors, [.invalidData])
         }
+    }
+
+    func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
+        let (sut, client) = makeSUT()
+        let invalidJSON = Data(_: "invalid json".utf8)
+        var capturedErrors = [RemoteFeedLoader.Error]()
+        sut.load { capturedErrors.append($0) }
+        client.complete(withStatusCode: 200, data: invalidJSON)
+        XCTAssertEqual(capturedErrors, [.invalidData])
     }
 
     // MARK: - Helpers
@@ -73,14 +82,14 @@ class RemoteFeedLoaderTests: XCTestCase {
             messages[index].completion(.failure(error))
         }
 
-        func complete(withStatusCode code: Int, at index: Int = 0) {
+        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
             let response = HTTPURLResponse(
                 url: requestedURLs[index],
                 statusCode: code,
                 httpVersion: nil,
                 headerFields: nil
             )!
-            messages[index].completion(.success(response))
+            messages[index].completion(.success(data, response))
         }
     }
 }
