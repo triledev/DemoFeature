662cd3bc8e4e1b02be1278ac2ac2474075bcdc9a
triledev <tri.le.mail@gmail.com>
2022-08-17 09:43:17 -0500

    (put UI dev on standby) to be continue later

--- DemoFeature/FeedViewModel.swift
+++ DemoFeature/FeedViewModel.swift
@@ -10,11 +10,6 @@ import Foundation
 class FeedViewModel: ObservableObject {
     @Published var stories: [FeedItem?] = Array(repeating: nil, count: 10)
 
-    func fetchTopStories() {
-        
-    }
-
     func fetchStory(withID id: Int, completion: @escaping (FeedItem?) -> Void) {
-        
     }
 }
