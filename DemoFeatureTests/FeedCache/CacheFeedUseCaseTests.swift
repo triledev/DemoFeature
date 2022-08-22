987425ebebb8ee54f79e7de742830aa46e144284
triledev <tri.le.mail@gmail.com>
2022-08-22 16:59:21 -0500

    Save command requests cache deletion

--- DemoFeatureTests/FeedCache/CacheFeedUseCaseTests.swift
+++ DemoFeatureTests/FeedCache/CacheFeedUseCaseTests.swift
@@ -6,15 +6,26 @@
 //
 
 import XCTest
+import DemoFeature
 
 class LocalFeedLoader {
+    private let store: FeedStore
+
     init(store: FeedStore) {
-        
+        self.store = store
+    }
+
+    func save(_ items: [FeedItem]) {
+        store.deleteCacheFeed()
     }
 }
 
 class FeedStore {
     var deleteCacheFeedCallCount = 0
+
+    func deleteCacheFeed() {
+        deleteCacheFeedCallCount += 1
+    }
 }
 
 class CacheFeedUseCaseTests: XCTestCase {
@@ -23,5 +34,25 @@ class CacheFeedUseCaseTests: XCTestCase {
         let store = FeedStore()
         _ = LocalFeedLoader(store: store)
         XCTAssertEqual(store.deleteCacheFeedCallCount, 0)
+    }
+
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+
+        let items = [uniqueItem(), uniqueItem()]
+        sut.save(items)
+
+        XCTAssertEqual(store.deleteCacheFeedCallCount, 1)
+    }
+    
+    // MARK: - Helopers
+
+    private func uniqueItem() -> FeedItem {
+        return FeedItem(author: "John Doe", title: "any", description: "any", url: anyURL(), source: "any", imageURL: anyURL(), category: "any", language: "en", country: "us", published: "any")
+    }
+
+    private func anyURL() -> URL {
+        return URL(string: "http://any-url.com")!
     }
 }
