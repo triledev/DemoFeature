ad660517024926b6aa0b6c727342fdd5de35e6ef
Tri Le <tri.le.mail@gmail.com>
2022-08-26 18:50:12 -0500

    Move `RemoteFeedItem` to its own file.

--- DemoFeature/FeedAPI/FeedItemMapper.swift
+++ DemoFeature/FeedAPI/FeedItemMapper.swift
@@ -7,31 +7,7 @@
 
 import Foundation
 
-internal struct RemoteFeedItem: Decodable {
-    internal let author: String?
-    internal let title: String?
-    internal let description: String?
-    internal let url: URL?
-    internal let source: String?
-    internal let imageURL: URL?
-    internal let category: String?
-    internal let language: String?
-    internal let country: String?
-    internal let publishedAt: String?
 
-    private enum CodingKeys: String, CodingKey {
-        case author = "author"
-        case title = "title"
-        case description = "description"
-        case url = "url"
-        case source = "source"
-        case imageURL = "image"
-        case category = "category"
-        case language = "language"
-        case country = "country"
-        case publishedAt = "published_at"
-    }
-}
 
 internal final class FeedItemsMapper {
     private struct Root: Decodable {
