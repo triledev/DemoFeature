9353853ac2e4742ec3bed514a4ceb3d742280a87
Tri Le <tri.le.mail@gmail.com>
2022-08-26 16:40:07 -0500

    Move `FeedStore` to its own file.

---
+++ DemoFeature/FeedCache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  DemoFeature
+//
+//  Created by Tri Le on 8/26/22.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+
+    func deleteCacheFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
