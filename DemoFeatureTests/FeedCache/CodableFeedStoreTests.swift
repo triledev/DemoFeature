54d8cbe7f383bef8c1483770fbd27b6e76f9b12b
Tri Le <tri.le.mail@gmail.com>
2022-09-01 18:34:23 -0500

    Extract duplicate store URL creation into a helper factory method.

--- DemoFeatureTests/FeedCache/CodableFeedStoreTests.swift
+++ DemoFeatureTests/FeedCache/CodableFeedStoreTests.swift
@@ -78,14 +78,13 @@ class CodableFeedStoreTests: XCTestCase {
     override func setUp() {
         super.setUp()
 
-        let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("item-feed.store")
-        try? FileManager.default.removeItem(at: storeURL)
+        try? FileManager.default.removeItem(at: storeURL())
     }
+
     override func tearDown() {
         super.tearDown()
 
-        let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("item-feed.store")
-        try? FileManager.default.removeItem(at: storeURL)
+        try? FileManager.default.removeItem(at: storeURL())
     }
 
     func test_retrieve_deliversEmptyOnEmptyCache() {
@@ -153,9 +152,12 @@ class CodableFeedStoreTests: XCTestCase {
     // MARK: - Helpers
 
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
-        let storeURL  = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("item-feed.store")
-        let sut = CodableFeedStore(storeURL: storeURL)
+        let sut = CodableFeedStore(storeURL: storeURL())
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
+    }
+    
+    private func storeURL() -> URL {
+        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("item-feed.store")
     }
 }
