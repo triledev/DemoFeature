bb1f1c0e251db536030d19ac564487d03cff2360
Tri Le <tri.le.mail@gmail.com>
2022-09-01 10:05:03 -0500

    Retrieving from empty cache twice delivers same empty result (no side-effects).

--- DemoFeatureTests/FeedCache/CodableFeedStoreTests.swift
+++ DemoFeatureTests/FeedCache/CodableFeedStoreTests.swift
@@ -12,6 +12,10 @@ class CodableFeedStore {
     func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
         completion(.empty)
     }
+
+    func insert(_ feed: [LocalFeedItem], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
+        completion(nil)
+    }
 }
 
 class CodableFeedStoreTests: XCTestCase {
@@ -33,4 +37,47 @@ class CodableFeedStoreTests: XCTestCase {
 
         wait(for: [exp], timeout: 1.0)
     }
+
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+        let sut = CodableFeedStore()
+        let exp = expectation(description: "Wait for cache retrieval")
+
+        sut.retrieve { firstResult in
+            sut.retrieve { secondResult in
+                switch (firstResult, secondResult) {
+                case (.empty, .empty):
+                    break
+                default:
+                    XCTFail("Expected retrieving twice from empty cache to deliver same empty result, got \(firstResult) and \(secondResult) instead")
+                }
+                exp.fulfill()
+            }
+        }
+
+        wait(for: [exp], timeout: 1.0)
+    }
+
+//    func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValues() {
+//        let sut = CodableFeedStore()
+//        let feed = uniqueItemFeed().local
+//        let timestamp = Date()
+//        let exp = expectation(description: "Wait for cache retrieval")
+//
+//        sut.insert(feed, timestamp: timestamp) { insertError in
+//            XCTAssertNil(insertionError, "Expected feed to be inserted successfully")
+//
+//            sut.retrieve { retrieveResult in
+//                switch retrieveResult {
+//                case let .found(retrievedFeed, retrievedTimestamp):
+//                    XCTAssertEqual(retrievedFeed, feed)
+//                    XCTAssertEqual(retrievedTimestamp, timestamp)
+//                default:
+//                    XCTFail("Expected found result with feed \(feed) and timestamp \(timestamp), got \(retrieveResult) instead")
+//                }
+//                exp.fulfill()
+//            }
+//        }
+//
+//        wait(for: [exp], timeout: 1.0)
+//    }
 }
