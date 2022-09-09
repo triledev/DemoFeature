b4b88488d792638ecf60d331f3ec5f2fa8e4fd10
Tri Le <tri.le.mail@gmail.com>
2022-09-09 15:04:13 -0500

    Notify to the client via comment of their responsibility to dispatch to appropriate thread if neede.

--- DemoFeature/FeedCache/FeedStore.swift
+++ DemoFeature/FeedCache/FeedStore.swift
@@ -18,7 +18,15 @@ public protocol FeedStore {
     typealias InsertionCompletion = (Error?) -> Void
     typealias RetrievalCompletion = (RetrieveCachedFeedResult) -> Void
 
+    /// The completion handler can be invoked in any thread.
+    /// Client are responsible for dispatch to appropriate threads, if needed.
     func deleteCacheFeed(completion: @escaping DeletionCompletion)
+
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible for dispatch to appropriate threads, if needed.
     func insert(_ items: [LocalFeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible for dispatch to appropriate threads, if needed.
     func retrieve(completion: @escaping RetrievalCompletion)
 }
