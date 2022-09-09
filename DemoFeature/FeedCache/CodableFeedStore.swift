1929f330134f0bd1fec5834265e1b12f7e827b59
Tri Le <tri.le.mail@gmail.com>
2022-09-09 14:57:56 -0500

    Make  queue concurrent to allow multiple  to be processed in parallel (since it has no side-effects) and use  when performing side-effects to guarantee data consistency and avoid race conditions.

--- DemoFeature/FeedCache/CodableFeedStore.swift
+++ DemoFeature/FeedCache/CodableFeedStore.swift
@@ -47,7 +47,7 @@ public class CodableFeedStore: FeedStore {
         }
     }
 
-    private let queue = DispatchQueue(label: "\(CodableFeedStore.self)Queue", qos: .userInitiated)
+    private let queue = DispatchQueue(label: "\(CodableFeedStore.self)Queue", qos: .userInitiated, attributes: .concurrent)
     private let storeURL: URL
 
     public init(storeURL: URL) {
@@ -73,7 +73,7 @@ public class CodableFeedStore: FeedStore {
 
     public func insert(_ feed: [LocalFeedItem], timestamp: Date, completion: @escaping InsertionCompletion) {
         let storeURL = self.storeURL
-        queue.async {
+        queue.async(flags: .barrier) {
             do {
                 let encoder = JSONEncoder()
                 let cache = Cache(feed: feed.map(CodableFeedItem.init), timestamp: timestamp)
@@ -88,7 +88,7 @@ public class CodableFeedStore: FeedStore {
 
     public func deleteCacheFeed(completion: @escaping DeletionCompletion) {
         let storeURL = self.storeURL
-        queue.async {
+        queue.async(flags: .barrier) {
             guard FileManager.default.fileExists(atPath: storeURL.path) else {
                 return completion(nil)
             }
