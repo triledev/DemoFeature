1bca3848fe21073325f03340d54dd64916de33b5
Tri Le <tri.le.mail@gmail.com>
2022-09-09 14:39:08 -0500

    Proved that the `CodableFeedStore` side-effects run serially.

--- DemoFeatureTests/FeedCache/CodableFeedStoreTests.swift
+++ DemoFeatureTests/FeedCache/CodableFeedStoreTests.swift
@@ -125,6 +125,32 @@ class CodableFeedStoreTests: XCTestCase {
         XCTAssertNotNil(deletionError, "Expected cache deletion to fail")
     }
 
+    func test_storeSideEffects_runSerially() {
+        let sut = makeSUT()
+        var completedOperationsInOrder = [XCTestExpectation]()
+
+        let op1 = expectation(description: "Operation 1")
+        sut.insert(uniqueItemFeed().local, timestamp: Date()) { _ in
+            completedOperationsInOrder.append(op1)
+            op1.fulfill()
+        }
+
+        let op2 = expectation(description: "Operation 2")
+        sut.deleteCacheFeed { _ in
+            completedOperationsInOrder.append(op2)
+            op2.fulfill()
+        }
+
+        let op3 = expectation(description: "Operation 3")
+        sut.insert(uniqueItemFeed().local, timestamp: Date()) { _ in
+            completedOperationsInOrder.append(op3)
+            op3.fulfill()
+        }
+
+        waitForExpectations(timeout: 5.0)
+        XCTAssertEqual(completedOperationsInOrder, [op1, op2, op3], "Expected side-effects to run serially but operations finished in the wrong order")
+    }
+
     // MARK: - Helpers
 
     private func makeSUT(storeURL: URL? = nil, file: StaticString = #filePath, line: UInt = #line) -> FeedStore {
@@ -200,6 +226,8 @@ class CodableFeedStoreTests: XCTestCase {
 
     /*
      .userDomainMask work for macOS framework target.
+     The cache directory permission is different in the iOS simulator
+     (you can delete the directory on the simulator, but not on the macOS target!)
      */
     private func cachesDirectory() -> URL {
         return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
