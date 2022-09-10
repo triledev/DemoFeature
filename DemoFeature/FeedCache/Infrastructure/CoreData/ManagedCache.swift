ec5235a715889b310793f24df86e8c14711a4d27
Tri Le <tri.le.mail@gmail.com>
2022-09-09 19:36:58 -0500

    Add `ManagedCache` and `ManagedFeedItem` model representations.

---
+++ DemoFeature/FeedCache/Infrastructure/CoreData/ManagedCache.swift
@@ -0,0 +1,29 @@
+//
+//  ManagedCache.swift
+//  DemoFeature
+//
+//  Created by Tri Le on 9/9/22.
+//
+
+import CoreData
+
+@objc(ManageCache)
+internal class ManagedCache: NSManagedObject {
+    @NSManaged internal var timestamp: Date
+    @NSManaged internal var feed: NSOrderedSet
+}
+
+@objc(ManagedFeedImage)
+internal class ManageFeedImage: NSManagedObject {
+    @NSManaged internal var author: String?
+    @NSManaged internal var title: String?
+    @NSManaged internal var itemDescription: String?
+    @NSManaged internal var url: URL?
+    @NSManaged internal var source: String?
+    @NSManaged internal var image: URL?
+    @NSManaged internal var category: String?
+    @NSManaged internal var language: String?
+    @NSManaged internal var country: String?
+    @NSManaged internal var publishedAt: String?
+    @NSManaged internal var cache: ManagedCache
+}
