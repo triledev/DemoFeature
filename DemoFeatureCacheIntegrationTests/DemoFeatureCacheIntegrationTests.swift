15eb81ffaca69556f79c6f3842ec19c788135d67
Tri Le <tri.le.mail@gmail.com>
2022-09-20 15:49:16 -0500

    Add `DemoFeatureCacheIntegrationTests` target to separate the potentially-slower cache integration tests from the fast unit/isolated tests

---
+++ DemoFeatureCacheIntegrationTests/DemoFeatureCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  DemoFeatureCacheIntegrationTests.swift
+//  DemoFeatureCacheIntegrationTests
+//
+//  Created by Tri Le on 9/20/22.
+//
+
+import XCTest
+
+class DemoFeatureCacheIntegrationTests: XCTestCase {
+
+}
